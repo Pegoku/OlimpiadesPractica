1dbb4d4680f8a955adfd6436ab23f09ab54054ce
Pegoku <pegoku08@gmail.com>
2025-01-24 12:16:36 +0100

    commit

--- 4.py
+++ 4.py
@@ -1,29 +1,22 @@
 nCycles = int(input())
-
 for _ in range(nCycles):
     inValues = input()
     nums = inValues.split(" ")
-    # numsSet = set(nums)
-    # if len(numsSet) != len(nums):
-    #     print("PARANORMAL") 
-    #     continue
-    
-    numsInt = []
-    for i in nums:
-        numsInt.append(int(i))
-    # print(numsInt)
+    if nums == ['0']:
+        print("NORMAL")
+        continue
+    numsInt = [int(a) for a in nums if a!='0']
     llista = []
     for a in numsInt:
         if a > 0:
             llista.append(a)
-        elif a < 0:
-            if llista and llista[-1] == abs(a):
+        else:
+            if len(llista) > 0 and llista[-1] == abs(a):
                 llista.pop()
-    #     print(llista)
-    # print(llista)
+            else:
+                llista = [1]
+                break
     if len(llista) == 0:
         print("NORMAL")
     else:
         print("PARANORMAL")
-
-exit(0)
